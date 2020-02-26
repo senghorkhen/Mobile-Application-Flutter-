30058cf85faf622912c6b2752b784d5e5605196c
senghor.khen <senghor.khen@pnc.lan>
2020-02-26 18:04:08 +0700

    counter

--- main.dart
+++ main.dart
@@ -1,13 +1,20 @@
-import 'package:api_user/user.dart';
-import 'package:flutter/material.dart';
 
-void main() => runApp(Home());
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:app_counter/counter.dart';
+void main() => runApp(Senghor());
+class Senghor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: User(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: Text("Counter App"),
+        ),
+        body: Learning(),
+      ),
     );
   }
 }
