a8010184a45426ab734a137bf4cbe65a8874ce0a
senghor.khen <senghor.khen@pnc.lan>
2020-03-05 12:55:55 +0700

    float btn

--- main.dart
+++ main.dart
@@ -1,152 +1,157 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+// import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-void main() => runApp(NiceApp());
-class NiceApp extends StatelessWidget {
+void main() => runApp(MobileBtn());
+
+class MobileBtn extends StatefulWidget {
+  @override
+  _MobileBtnState createState() => _MobileBtnState();
+}
+
+class _MobileBtnState extends State<MobileBtn> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text("My Love", style: TextStyle(color: Colors.pink[500], fontFamily: "project", fontSize: 30.0),),
-          actions: <Widget>[
-            Container(
-              child: Image.asset("assets/images/logo.png", width: 130.0,),
-            ),
-          ],
-          iconTheme: IconThemeData(color: Colors.pink[500]),
+          title: Text("Floating Button"),
         ),
-        drawer: Drawer(
-          child: ListView(
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: Icon(Icons.add),
+        //   backgroundColor: Colors.pink,
+        //   // mini: true,
+        //   // shape: RoundedRectangleBorder(
+        //   //   borderRadius: BorderRadius.all(Radius.circular(12.0))
+        //   // ),
+        // ),
+        // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      //  floatingActionButton: FloatingActionButton.extended(
+      //    onPressed: () {},
+      //    icon: Icon(Icons.message),
+      //    label: Text("Message"),
+      //    backgroundColor: Colors.pink,
+      //    shape: RoundedRectangleBorder(
+      //      borderRadius: BorderRadius.all(Radius.circular(12.0))
+      //    ),
+      //  ),
+
+      // floatingActionButton: SpeedDial(
+      //   animatedIcon: AnimatedIcons.menu_close,
+      //   overlayColor: Colors.pink,
+      //   overlayOpacity: 0.3,
+
+      //   children: [
+      //     SpeedDialChild(
+      //       child: Icon(Icons.message),
+      //       backgroundColor: Colors.teal,
+      //       label: "Message",
+      //       labelBackgroundColor: Colors.teal,
+      //       labelStyle: TextStyle(
+      //         color: Colors.white,
+      //         fontSize: 16.0,
+      //       ),
+      //     ),
+      //     SpeedDialChild(
+      //       child: Icon(Icons.group),
+      //       backgroundColor: Colors.pink,
+      //       label: "Group",
+      //       labelBackgroundColor: Colors.pink,
+      //       labelStyle: TextStyle(
+      //         color: Colors.white,
+      //         fontSize: 16.0,
+      //       ),
+      //     ),
+      //     SpeedDialChild(
+      //       child: Icon(Icons.share),
+      //       backgroundColor: Colors.red,
+      //       label: "Searing",
+      //       labelBackgroundColor: Colors.red,
+      //       labelStyle: TextStyle(
+      //         color: Colors.white,
+      //         fontSize: 16.0,
+      //       ),
+      //     ),
+      //     SpeedDialChild(
+      //       child: Icon(Icons.favorite),
+      //       backgroundColor: Colors.orange,
+      //       label: "Favorite",
+      //       labelBackgroundColor: Colors.orange,
+      //       labelStyle: TextStyle(
+      //         color: Colors.white,
+      //         fontSize: 16.0,
+      //       ),
+      //     ),
+      //   ],
+      // ),
+
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 60.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              DrawerHeader(
-                child: Center(
-                  child: CircleAvatar(
-                    radius: 60.0,
-                    backgroundImage: AssetImage("assets/images/profile.jpg"),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/bg.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
+            MaterialButton(
+              onPressed: () {},
+              child: Column(
+                 mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Icon(Icons.home),
+                  Text("Home"),
+                ],
               ),
-            ],
+            ),
+
+              MaterialButton(
+              onPressed: () {},
+              child: Column(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(Icons.person),
+                  Text("Person"),
+                ],
+              ),
+            ),
+
+              MaterialButton(
+              onPressed: () {},
+              child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.favorite),
+                  Text("Favorite"),
+                ],
+              ),
+            ),
+
+              MaterialButton(
+              onPressed: () {},
+              child: Column(
+                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Icon(Icons.search),
+                  Text("Searching"),
+                ],
+              ),
+            ),
+
+          ],
           ),
         ),
-        body: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                child: Image.asset("assets/images/1.jpg"),
-              ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.favorite_border, color: Colors.pink[500], size: 50.0,),
-                  Text(
-                    "Smile For Life",
-                    style: TextStyle(
-                      color: Colors.pink[500],
-                      fontFamily: "project",
-                      fontSize: 30.0
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                "Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer.",
-                style: TextStyle(fontSize: 17.0),
-              ),
-            ),
-            Container(
-              child: RaisedButton(
-                child: Text("Enjoy Our Project",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontFamily: "project",
-                    color: Colors.white,
-                  ),
-                ),
-                color: Colors.pink[500],
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                ),
-                splashColor: Colors.green,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top:15.0),
-              child: Image.asset("assets/images/2.jpg"),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 20.0),
-              child: Row(
-                children: <Widget>[
-                 Expanded(
-                   child: Column(
-                     children: <Widget>[
-                        Icon(Icons.favorite_border, color: Colors.pink[500],),
-                        Text("Loves", style: TextStyle(fontSize: 17.0),)
-                     ],
-                   ),
-                 ),
-                 Expanded(
-                   child: Column(
-                     children: <Widget>[
-                        Icon(Icons.thumb_up, color: Colors.pink[500],),
-                        Text("Likes", style: TextStyle(fontSize: 17.0),)
-                     ],
-                   ),
-                 ),
-                 Expanded(
-                   child: Column(
-                     children: <Widget>[
-                        Icon(Icons.share, color: Colors.pink[500],),
-                        Text("Shares", style: TextStyle(fontSize: 17.0),)
-                     ],
-                   ),
-                 ),
-                 Expanded(
-                   child: Column(
-                     children: <Widget>[
-                        Icon(Icons.comment, color: Colors.pink[500],),
-                        Text("Comments", style: TextStyle(fontSize: 17.0),)
-                     ],
-                   ),
-                 ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                "Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer.",
-                style: TextStyle(fontSize: 17.0),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton.extended(
+      ),
+
+      floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          label: Text("My Project", style: TextStyle(fontSize: 20.0, fontFamily: "Project"),),
-          icon: Icon(Icons.favorite),
-          backgroundColor: Colors.pink[500],
+          child: Icon(Icons.add),
+          backgroundColor: Colors.pink,
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      ) ,
+        // nv pi kram floatingactionbutton
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
     );
   }
 }
